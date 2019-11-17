001353892349ffe22e09cc6fbff155528480f333
Randy Steele <steelerx155@gmail.com>
2019-11-17 19:36:50 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,20 +1,4 @@
-# module Findable
-#   module ClassMethods
-    
-#   def self.find_by_name(name)
-#     @@artists.detect{|a| a.name == name}
-#   end
-  
-#   def self.find_by_name(name)
-#     @@songs.detect{|a| a.name == name}
-#   end
-#   module InstanceMethods
-#   def initialize
-#     self.class.all << self
-#   end
-# end
-# end
-# end
+
 
 
 
